db1ec5b51b74315aa665f8e6cf5ee0e996f9fb0e
ByoungilYoun <jimmy7525@naver.com>
2020-07-29 04:09:45 +0900

    Finish Onboarding UI

--- LoginWithFirebase/LoginWithFirebase/Controller/OnboardingController.swift
+++ LoginWithFirebase/LoginWithFirebase/Controller/OnboardingController.swift
@@ -16,6 +16,15 @@ class OnboardingController : UIViewController {
   private var onboardingItems = [OnboardingItemInfo]()
   private var onboardingView = PaperOnboarding()
   
+  private let getStartedButton : UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Get Started", for: .normal)
+    button.setTitleColor(.white, for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+    button.addTarget(self, action: #selector(dismissOnboarding), for: .touchUpInside)
+    return button
+  }()
+  
   //MARK: - Life Cycle
   
   override func viewDidLoad() {
@@ -25,19 +34,47 @@ class OnboardingController : UIViewController {
     
   }
   
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+  //MARK: - Selectors
+  @objc func dismissOnboarding() {
+    print("123")
+  }
+  
   //MARK: - Helpers
+  func animateGetStartedButton(_ shouldShow : Bool ) {
+    let alpha : CGFloat = shouldShow ? 1 : 0
+    UIView.animate(withDuration: 0.5) {
+      self.getStartedButton.alpha = alpha
+    }
+  }
+  
+  
   func configureUI() {
     view.addSubview(onboardingView)
     onboardingView.fillSuperview()
+    onboardingView.delegate = self
+    
+    view.addSubview(getStartedButton)
+    getStartedButton.alpha = 0
+    getStartedButton.centerX(inView: view)
+    getStartedButton.anchor(bottom : view.safeAreaLayoutGuide.bottomAnchor,
+                            paddingBottom: 128)
   }
   
   func configureOnboardingDataSource() {
-    let item1 = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "baseline_insert_chart_white_48pt").withRenderingMode(.alwaysOriginal), title: "Metrics", description: "Some description here ..", pageIcon: UIImage(), color: .systemPurple, titleColor: .white, descriptionColor: .white, titleFont: UIFont.boldSystemFont(ofSize: 24), descriptionFont: UIFont.systemFont(ofSize: 16))
+    let item1 = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "baseline_insert_chart_white_48pt").withRenderingMode(.alwaysOriginal), title: MSG_METRICS, description: MSG_ONBOARDING_METRICS, pageIcon: UIImage(), color: .systemPurple, titleColor: .white, descriptionColor: .white, titleFont: UIFont.boldSystemFont(ofSize: 24), descriptionFont: UIFont.systemFont(ofSize: 16))
+    
+     let item2 = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "baseline_notifications_active_white_48pt").withRenderingMode(.alwaysOriginal), title: MSG_NOTIFICATIONS, description: MSG_ONBOARDING_NOTIFICATIONS, pageIcon: UIImage(), color: .systemGreen, titleColor: .white, descriptionColor: .white, titleFont: UIFont.boldSystemFont(ofSize: 24), descriptionFont: UIFont.systemFont(ofSize: 16))
+    
+     let item3 = OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "baseline_dashboard_white_48pt").withRenderingMode(.alwaysOriginal), title: MSG_DASHBOARD, description: MSG_ONBOARDING_DASHBOARD, pageIcon: UIImage(), color: .systemBlue, titleColor: .white, descriptionColor: .white, titleFont: UIFont.boldSystemFont(ofSize: 24), descriptionFont: UIFont.systemFont(ofSize: 16))
     
     
     onboardingItems.append(item1)
-    onboardingItems.append(item1)
-    onboardingItems.append(item1)
+    onboardingItems.append(item2)
+    onboardingItems.append(item3)
     
     onboardingView.dataSource = self
     onboardingView.reloadInputViews()
@@ -54,6 +91,11 @@ extension OnboardingController : PaperOnboardingDataSource {
   func onboardingItem(at index: Int) -> OnboardingItemInfo {
     return onboardingItems[index]
   }
-  
-  
+}
+
+extension OnboardingController : PaperOnboardingDelegate {
+  func onboardingWillTransitonToIndex(_ index : Int) {
+    let shouldShow = index == onboardingItems.count - 1 ? true : false
+    animateGetStartedButton(shouldShow)
+  }
 }
---
+++ LoginWithFirebase/LoginWithFirebase/Utils/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  LoginWithFirebase
+//
+//  Created by 윤병일 on 2020/07/29.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import Foundation
+
+let MSG_METRICS = "Metrics"
+let MSG_DASHBOARD = "Dashboard"
+let MSG_NOTIFICATIONS = "Get Notified"
+
+let MSG_ONBOARDING_METRICS = "Extract valuable insights and come up with data driven product initiatives to help grow your business"
+let MSG_ONBOARDING_NOTIFICATIONS = "Get notified when important stuff is happening, so you don't miss out on the action"
+let MSG_ONBOARDING_DASHBOARD = "Everything you need all in one place, available through our dashboard feature"
